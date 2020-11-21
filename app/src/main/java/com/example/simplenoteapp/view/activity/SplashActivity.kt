99ef142afb7d906eb1584aeadde731e5801bdc12
YusufCanerGures <gurescaner@gmail.com>
2020-11-21 15:27:07 +0300

    SplashScreen added.

--- app/src/main/java/com/example/simplenoteapp/view/activity/SplashActivity.kt
+++ app/src/main/java/com/example/simplenoteapp/view/activity/SplashActivity.kt
@@ -1,12 +1,23 @@
 package com.example.simplenoteapp.view.activity
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 import com.example.simplenoteapp.R
+import com.example.simplenoteapp.util.extStartActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+        object : CountDownTimer(6000, 1000) {
+            override fun onFinish() {
+                this@SplashActivity extStartActivity MainActivity::class.java
+            }
+
+            override fun onTick(millisUntilFinished: Long) {}
+        }.start()
     }
 }
